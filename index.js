78fe33d8214d351603cd852bfe12fb2f2a1d08e9
faiyaj01 <faiyajahmed710@gmail.com>
2023-06-23 16:38:20 +0600

    Added the temperature , description and the zone

--- index.js
+++ index.js
@@ -6,6 +6,17 @@ window.addEventListener("load", () => {
       console.log(position);
       longituade = position.coords.longitude;
       latitude = position.coords.latitude;
+      apiId = "3f545d72c679d214b0570549993af882";
+      api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longituade}&appid=${apiId}&units=metric`;
+      console.log(api);
+      fetch(api)
+        .then((data) => {
+          return data.json();
+        })
+        .then((data) => {
+          console.log(data);
+          let { main, name, weather } = data;
+        });
     });
   }
 });
